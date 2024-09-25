5cd21036ec2df072c0dd7f3c23e45a5824574b72
LesterArvidAnastacioCPE11S1 <qlapanastacio@tip.edu.ph>
2024-09-25 12:24:13 +0800

    Add files via upload

---
+++ Calculator (switch).cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cwchar>
+#include <windows.h>
+#include <iomanip>
+using namespace std;
+using std:: setw;
+
+int main(){
+	char op;
+	float sum;
+	float value1;
+	float value2;
+	char cont = 'Y';
+	char stop = 'N';
+	
+	while (cont == 'Y'){
+	cout << setw(65)<<"BASIC CALCULATOR"<< endl;
+	cout << "Choose Operator: " << endl;
+	cout << "[A] Add, [B] Sub, [C] Multi, [D] Div"<< endl;
+	cin >> op;
+	
+	switch(op){
+		case 'A':
+			cout << "Please Enter First Number: "<< endl;
+			cin >> value1;
+			cout << "Please Enter Second Number"<< endl;
+			cin >> value2;
+			sum = value1 + value2;
+			cout << "The answer is.. :" << sum << endl;
+			break;
+		case 'B':
+			cout << "Please Enter First Number: "<< endl;
+			cin >> value1;
+			cout << "Please Enter Second Number"<< endl;
+			cin >> value2;
+			sum = value1 - value2;
+			cout << "The answer is.. :" << sum << endl;
+			break;
+		case 'C':
+			cout << "Please Enter First Number: "<< endl;
+			cin >> value1;
+			cout << "Please Enter Second Number"<< endl;
+			cin >> value2;
+			sum = value1 * value2;
+			cout << "The answer is.. :" << sum << endl;
+			break;
+		case 'D':
+			cout << "Please Enter First Number: "<< endl;
+			cin >> value1;
+			cout << "Please Enter Second Number"<< endl;
+			cin >> value2;
+			sum = value1 / value2;
+			cout << "The answer is.. :" << sum << endl;
+			break;
+		default:{
+			cout << "Unidentified";
+			break;
+		}
+
+	}
+	cout << "Try Again? Y/N: "<< endl;
+	cin >> cont;
+	if ('N'){
+		cout << "Thank you for using the calculator";
+	}
+}
+	return 0;
+}
